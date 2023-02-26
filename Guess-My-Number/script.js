a734c360fa38ab2d68a7ddbbb7fbf906cb112ad8
prof-awis <awinoemanuel2000@gmail.com>
2023-02-26 23:16:22 +0300

    DOM manipulation

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent);
+
+/*DOM Manipulation(Document Object Model)
+- structured representation of html structure
+It allows JS to access the html elements and styles to manipulate them.
+- DOM !== JavaScript(DOM is not part of JS but it is a part of Web API[Application programming interface])
+*/
+
+document.querySelector('.message').textContent = 'Correct Number!';
+//elemetn changed to Correct Number!
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+document.querySelector('.guess').value = 23;
+document.querySelector('.guess').value;
