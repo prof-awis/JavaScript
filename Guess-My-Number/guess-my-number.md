e23cda873492d9de5391620f92a30475abd31d66
prof-awis <awinoemanuel2000@gmail.com>
2023-03-08 21:02:47 +0300

    .

--- Guess-My-Number/guess-my-number.md
+++ Guess-My-Number/guess-my-number.md
@@ -5,3 +5,5 @@ This will be the first real project I will be working on with JavaScript.
 It involves mainly DOM manipulation.
 
 Follow through to see how it works
+
+It will be a game that guesses a secret number between one and 20.
--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
 
 /*DOM Manipulation(Document Object Model)
 - structured representation of html structure
@@ -8,6 +8,7 @@ It allows JS to access the html elements and styles to manipulate them.
 - DOM !== JavaScript(DOM is not part of JS but it is a part of Web API[Application programming interface])
 */
 
+/*
 document.querySelector('.message').textContent = 'Correct Number!';
 //elemetn changed to Correct Number!
 
@@ -15,3 +16,17 @@ document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23;
 document.querySelector('.guess').value;
+*/
+const x = function () {
+  console.log(23);
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔No Number!';
+  }
+});
