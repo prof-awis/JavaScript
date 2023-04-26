a36867029a1367c1db4c258238db0f9ed92016a5
prof-awis <awinoemanuel2000@gmail.com>
2023-04-26 09:28:42 +0300

    Concept of hoisting

--- Behind the scenes/script.js
+++ Behind the scenes/script.js
@@ -39,3 +39,31 @@ const x = calcAge(1995);
 console.log(x);
 
 //! console.log(age); You cannot access the child scope
+
+//? Concept of hoisting
+
+//Hoisting with variables
+console.log(me);
+// console.log(job);Uncaught ReferenceError: Cannot access 'job' before initialization
+// console.log(year); the two are still in the temporary dead zone(TDZ) and hence cannot be accessed
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+
+//hoisting with functions
+console.log(addDecl(3, 5));
+// console.log(addExpr(1, 3)); Uncaught ReferenceError: Cannot access 'addExpr' before initialization
+// console.log(addArrow(5, 5)); Uncaught ReferenceError: Cannot access 'addArrow' before initialization
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+const addArrow = (a, b) => {
+  return a + b;
+};
